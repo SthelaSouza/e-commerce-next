0d2a589024d9819c7a60aaafd471948337f105e2
STHÉLA SOUZA <88801741+SthelaSouza@users.noreply.github.com>
2021-12-04 18:52:39 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  
+  env: {
+    "BASE_URL": "http://localhost:3000",
+    "MONGODB_URL": "mongodb+srv://dbsthela:dbsthela123@cluster0.foxxj.mongodb.net/ecommerceIFSthela?retryWrites=true&w=majority",
+
+    "ACESS_TOKEN_SECRET":"",
+    "REFRESH_TOKEN_SECRET":"74h+H4shCwBTBF]Sxzd&RNhf6K;Lr~d?8@X#WEnT"
+
+  }
+}
